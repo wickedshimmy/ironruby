b1c1d38fcaa4c7d984193beae29dc2e5a319dbb9
Matt Enright <awickedshimmy@gmail.com>
2010-07-19 13:04:11 +0200

    Assign the type before attempting to lookup.

--- Runtime/Microsoft.Dynamic/Actions/DefaultBinder.GetMember.cs
+++ Runtime/Microsoft.Dynamic/Actions/DefaultBinder.GetMember.cs
@@ -212,7 +212,7 @@ namespace Microsoft.Scripting.Actions {
                 if (typeof(TypeTracker).IsAssignableFrom(type)) {
                     // ensure we don't have a non-generic type, and if we do report an error now.  This matches
                     // the rule version of the default binder but should probably be removed long term
-                    Type x = ((TypeTracker)target.Value).Type;
+                    type = ((TypeTracker)target.Value).Type;
                 } else if (type.IsInterface) {
                     // all interfaces have object members
                     type = typeof(object);
